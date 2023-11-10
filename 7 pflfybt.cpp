23e8bdb006a9ca30323b00ceae8ed1c87644d799
Gleb <144655536+Glen907@users.noreply.github.com>
2023-11-10 14:30:26 +0300

    7 pflfybt.cpp

--- 7 pflfybt.cpp
+++ 7 pflfybt.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 
-void int_massiv(){
-    
+int main()
+{
     setlocale(LC_ALL, "RU");
     srand(time(NULL));
     
     const int SIZE = 10;
-    int arr {SIZE};
+    int arr [SIZE];
     
     bool alredyThere;
     
     for (int i = 0; i < SIZE; i++)
     {
+        alredyThere = false;
+        int newRanomValue = rand()% 20;
+        
         arr[i] = rand () % 20;
+        for (int j = 0; j < i; j++)
+        {
+           if(arr[j]==newRanomValue) {
+               alreadythere = true;
+           }
+        }
+        if (alreadythere)
     }
     
-    for (int i = 0; i < SIZE; i++)
+    
+    
+    for (int q = 0; q < SIZE; q++)
     {
-        cout << arr[i] << endl;
+        cout << arr[q] << endl;
     }
     
     return 0;
