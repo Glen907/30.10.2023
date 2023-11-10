4c88d35c9d77c526ec4190011d6463690f7a2811
Gleb <144655536+Glen907@users.noreply.github.com>
2023-11-10 16:50:46 +0300

    7.1 задание.cpp

--- 7 pflfybt.cpp
+++ 7 pflfybt.cpp
@@ -1,37 +1,64 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
-int main()
-{
-    setlocale(LC_ALL, "RU");
-    srand(time(NULL));
-    
-    const int SIZE = 10;
-    int arr [SIZE];
-    
-    bool alredyThere;
-    
-    for (int i = 0; i < SIZE; i++)
-    {
-        alredyThere = false;
-        int newRanomValue = rand()% 20;
-        
-        arr[i] = rand () % 20;
-        for (int j = 0; j < i; j++)
-        {
-           if(arr[j]==newRanomValue) {
-               alreadythere = true;
-           }
-        }
-        if (alreadythere)
-    }
-    
-    
-    
-    for (int q = 0; q < SIZE; q++)
-    {
-        cout << arr[q] << endl;
-    }
-    
-    return 0;
+int main() {
+	int sizeOne[10], sizeTwo[20];
+	srand(time(0));
+
+	bool removeRepeat;
+
+	for (int i = 0; i < 10;)
+	{
+		removeRepeat = false;
+		int randomValue = rand() % 10;
+
+		for (int y = 0; y < i; y++)
+		{
+			if (sizeOne[y] == randomValue)
+			{
+				removeRepeat = true;
+				break;
+			}
+		}
+
+		if (!removeRepeat)
+		{
+			sizeOne[i] = randomValue;
+			i++;
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << sizeOne[i] << " ";
+	}
+
+	cout << endl;
+
+	for (int u = 0; u < 20;)
+	{
+		removeRepeat = false;
+		int randomValue = rand() % 20;
+
+		for (int t = 0; t < u; t++)
+		{
+			if (sizeTwo[t] == randomValue)
+			{
+				removeRepeat = true;
+				break;
+			}
+		}
+		if (!removeRepeat)
+		{
+			sizeTwo[u] = randomValue;
+			u++;
+		}
+
+	}
+
+	for (int u = 0; u < 20; u++)
+	{
+		cout << sizeTwo[u] << " ";
+	}
 }
